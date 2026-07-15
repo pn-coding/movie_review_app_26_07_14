17ceec0084d5e26dec6b116ecab1e615cb5ea85e
pncoding <pncoding@naver.com>
2026-07-15 13:43:25 +0900

    add: header & footer & hero section component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,29 @@
+import { Link } from "react-router-dom";
+
 export default function Header() {
-  return <header></header>;
+  return (
+    <header
+      className="
+        px-[20px] lg:px-[80px] xl:px-[200px] 
+        py-6 flex justify-between items-center
+        fixed top-0 left-0
+        w-full
+        z-10
+    "
+    >
+      <div className="text-red-500 font-bold text-lg xl:text-2xl tracking-tight">
+        {/* tracking: 자간 조절 */}
+        <Link to={"/"}>PNMOVIE</Link>
+      </div>
+
+      <ul className="flex gap-[80px] font-bold">
+        <li className="hover:text-red-500 transition">
+          <Link to={"/"}>HOME</Link>
+        </li>
+        <li className="hover:text-red-500 transition">
+          <Link to={"/search"}>SEARCh</Link>
+        </li>
+      </ul>
+    </header>
+  );
 }
